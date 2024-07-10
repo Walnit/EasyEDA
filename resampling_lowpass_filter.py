4332d5a061e6af7baa7836f4e8f5caff75a7af13
Nam <ducnamtran07@gmail.com>
2024-07-10 17:32:11 +0800

    I peaked today

--- resampling_lowpass_filter.py
+++ resampling_lowpass_filter.py
@@ -5,7 +5,7 @@ from scipy import signal
 from scipy import stats
 from cvxEDA import cvxEDA
 
-df = pd.read_csv("file_4.csv")[1:]
+df = pd.read_csv("file_2.csv")[1:]
 
 duration = 600
 og_sample_rate = len(df)/duration
@@ -30,6 +30,15 @@ eda_filtered = signal.filtfilt(b, a, eda_resampled)
 
 [r, p, t, l, d, e, obj] = cvxEDA(stats.zscore(eda_filtered), 1./new_sample_rate)
 
+r_peak_count = 0
+
+for i in range(len(r)):
+    if((i!= 0) and (i!=len(r)-1)):
+        if((r[i-1] < r[i]) and (r[i+1] < r[i])):
+            r_peak_count += 1
+
+print(r_peak_count)
+print(np.std(r))
 # plt.legend(['eda_resampled', 'eda_filtered', 'eda_n', 'r', 'p', 't'])
 # plt.plot(eda_filtered)
 # plt.plot(eda_n)
@@ -40,7 +49,6 @@ plt.plot(p)
 plt.show()
 plt.plot(t)
 plt.show()
-# #
 
 
 
